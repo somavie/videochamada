cafaf86fe89540f0b443211f1cd12fbaa6f412a4
somavie <somavie1@gmail.com>
2025-03-09 12:38:30 +0100

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on("connection", (socket) => {
+  console.log("Novo usuário conectado:", socket.id);
+
+  socket.on("join", (room) => {
+    socket.join(room);
+    socket.to(room).emit("user-connected", socket.id);
+  });
+
+  socket.on("offer", (payload) => {
+    io.to(payload.target).emit("offer", payload);
+  });
+
+  socket.on("answer", (payload) => {
+    io.to(payload.target).emit("answer", payload);
+  });
+
+  socket.on("ice-candidate", (incoming) => {
+    io.to(incoming.target).emit("ice-candidate", incoming.candidate);
+  });
+});
+
+server.listen(5000, () => console.log("Servidor rodando na porta 5000"));
